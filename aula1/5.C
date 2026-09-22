d69339a2a1717c304c3b048423e70c22e9faeb5b
Mclarasantos001 <mclarasantos967@gmail.com>
2026-09-22 19:19:16 -0300

    Update 5.C

--- aula1/5.C
+++ aula1/5.C
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main () {
+    //Ler um número e mostrar sua tabuada de 1 a 10 usando "for"
+    int numero, i;
+
+    printf("\nDigite um número: ");
+    scanf("%d", &numero);
+
+    for (i = 1; i <= 10; i++) {
+        printf("%d * %d = %d\n", numero, i, numero * i);
+    }
+
+    return 0;
+}
