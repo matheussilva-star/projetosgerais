461f29233a918dcc3e4472e8eee6937ec9285559
matheussilva-star <matheus.silva050204@gmail.com>
2026-09-21 21:52:43 -0300

    Update 4.c

--- aula1/4.c
+++ aula1/4.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main () {
+    //Ler média e frequência e informar se o aluno foi aprovado
+    float media, frequencia;
+
+    printf("\nDigite a media: ");
+    scanf("%f", &media);
+
+    printf("Digite a frequencia: ");
+    scanf("%f", &frequencia);
+
+    if (media >= 6.0 && frequencia >= 75.0f) {
+        printf("Aluno aprovado!\n");
+    }
+
+    else {
+        printf("Aluno reprovado!\n");
+    }
+
+    return 0;
+}
