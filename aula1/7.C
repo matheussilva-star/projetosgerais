500fd733b22c357b9f7f323442311ff0b7c1375b
pr462170-afk <pr462170@gmail.com>
2026-09-22 19:53:50 -0300

    Update 7.C

--- aula1/7.C
+++ aula1/7.C
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main () {
+
+    //Ler uma idade e informar se a pessoa é maior ou menor de idade
+    int idade;
+    
+    printf("\nDigite sua idade: ");
+    scanf("%d", &idade);
+
+    if (idade >= 18) {
+        printf("Maior de idade");
+    }
+
+    else {
+        printf("Menor de idade");
+    }
+
+    return 0;
+}
