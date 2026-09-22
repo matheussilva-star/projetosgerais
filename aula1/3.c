d1e981da17189c27dc1ef615139fb4a766f05cc0
matheussilva-star <matheus.silva050204@gmail.com>
2026-09-21 21:51:51 -0300

    Update 3.c

--- aula1/3.c
+++ aula1/3.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main () {
+    //Ler números repetidamente até o usuário digitar 0
+    int numero;
+
+    printf("\nDigite um numero ou 0 para sair: ");
+    scanf("%d", &numero);
+
+    while (numero != 0) {
+        printf("Você digitou o numero %d\n", numero);
+
+        printf("Digite outro numero ou 0 para sair: ");
+        scanf("%d", &numero);
+    }
+
+    printf("Programa encerrado.\n");
+
+    return 0;
+}
