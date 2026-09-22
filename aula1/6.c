63b8df74a65167deefde5217b1fa4febdf05384d
Mclarasantos001 <mclarasantos967@gmail.com>
2026-09-22 19:20:47 -0300

    Update 6.c

--- aula1/6.c
+++ aula1/6.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main () {
+    //Mostrar os números de 1 a 20 usando while
+    int i = 1;
+
+    while (i <= 20) {
+        printf("%d\n", i);
+        i++;
+    }
+
+    return 0;
+}
