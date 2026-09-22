a856cbddd8f1098f5c9fd5e7ccada51fd7c46567
matheussilva-star <matheus.silva050204@gmail.com>
2026-09-21 21:47:40 -0300

    Update 1.c

--- aula1/1.c
+++ aula1/1.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+    int main() {
+
+    // Informar nome, idade e altura
+    int idade;
+
+    printf("Qual a sua idade? ");
+    scanf("%d", &idade);
+
+    printf("Idade informada: %d\n", idade);
+
+    char nome[100];
+
+    printf("\nDigite seu nome: ");
+    scanf("%49s", nome);
+
+    printf("Nome: %s\n", nome);
+
+    float altura;
+
+    printf("\nInforme sua altura: ");
+    scanf("%f", &altura);
+
+    printf("Altura informada: %.2f\n", altura);         
+
+    return 0;
+
+}
